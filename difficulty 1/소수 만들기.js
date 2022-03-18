1f477331e0491dd1064d0f71b22f656e7cc2ebeb
obzva <dngyng1000@gmail.com>
2022-03-18 19:25:37 +0900

    [fix] difficulty 1 - 소수 만들기 소수 필터링 알고리즘 수정 굳이 안 필요한 sums array 삭제, 바로 answer integer 내보내기

--- difficulty 1/소수 만들기.js
+++ difficulty 1/소수 만들기.js
@@ -1,25 +1,23 @@
 //에라토스테네스의 체
-const prime = array => {
-    let max = array[array.length-1], upperLimit = Math.sqrt(max);
-    for (let i = 2; i <= upperLimit; i ++) {
-        array = array.filter(x => x % i !== 0 && x !== i);
-    };
-    return array;
+function prime(x) {
+    for (let i = 2; i <= Math.sqrt(x); i ++) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-
 function solution(nums) {
-    nums.sort((a, b) => a - b); //nums 크기 순으로 sorting
-    
-    let sums = [];
-    
+    let answer = 0;
     for (let i = 0; i < nums.length - 2; i ++) { //3개 수 조합으로 합 array 생성
         for (let j = i + 1; j < nums.length - 1; j ++) {
             for (let k = j + 1; k < nums.length; k ++) {
-                sums.push(nums[i] + nums[j] + nums[k]);
+                if (prime(nums[i] + nums[j] + nums[k])) {
+                    answer ++;
+                }
             }
         }
-    };
-    
-    return prime(sums).length;
-};
+    }
+    return answer;
+}
