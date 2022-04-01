70bdc2b4ee943b34671a67e3cc3169ed250c1f66
obzva <dngyng1000@gmail.com>
2022-04-01 23:21:00 +0900

    [FIX]

--- difficulty 2/짝지어 제거하기.js
+++ difficulty 2/짝지어 제거하기.js
@@ -14,20 +14,81 @@
     }
 }*/
 
-const solution = (s, i = 0) => {
-    if (i === s.length - 1) {
+/*
+런타임에러
+
+const solution = (s) => {
+    answer = 0;
+    let i = 0;
+    const pairRemover = (s, i) => {
+        if (i === s.length - 1) {
+            return;
+        } else {
+            if (s[i] === s[i + 1]) {
+                if (s.length === 2) {
+                    answer = 1;
+                    return;
+                } else {
+                    s = s.slice(0, i).concat(s.slice(i + 2));
+                    i = 0;
+                    pairRemover(s, i);
+                }
+            } else {
+                i++;
+                pairRemover(s, i);
+            }
+        }  
+    }
+    pairRemover(s, i);
+    return answer;
+}*/
+
+/*
+시간초과
+
+const solution = s => {
+    let stack = [s[0]];
+    for (let i = 1; i < s.length; i++) {
+        let curr = s[i];
+        if (stack[0] === curr) {
+            stack.shift();
+        } else {
+            stack.unshift(curr)
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}*/
+
+/*
+시간초과: shift, unshift의 시간복잡도는 O(n)인데 반해 pop, push의 시간복잡도는 O(1)
+
+const solution = s => {
+    let stack = [s[0]];
+
+    for (let i = 1; i < s.length; i++) {
+        if (stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}*/
+
+//홀수 케이스 제외해주면 시간초과 안 함
+const solution = s => {
+    if (s.length % 2 === 1) {
         return 0;
     }
-    if (s[i] === s[i + 1]) {
-        s = s.slice(0, i).concat(s.slice(i + 2));
-        solution(s);
-    }
-    if (!s) {
-        return 1;
-    }
-    solution(s, i + 1);
-}
 
-let s = 'cdcd';
-solution(s);
-console.log(solution(s));
+    let stack = [s[0]];
+
+    for (let i = 1; i < s.length; i++) {
+        if (stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}
