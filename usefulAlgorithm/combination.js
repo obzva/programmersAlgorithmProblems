deba97aca2d1c1c21461434bbc550d72cd750842
obzva <dngyng1000@gmail.com>
2022-04-13 16:18:35 +0900

    [UPDATE]

--- usefulAlgorithm/combination.js
+++ usefulAlgorithm/combination.js
@@ -7,12 +7,5 @@ function combination(arr, num){
         const attach = combinations.map((combination) => [arr[i], ...combination]);
         res.push(...attach);
     }
-    /*forEach costs more time
-    arr.forEach((v, idx, arr) => {
-      const rest = arr.slice(idx+1);
-      const combinations = combination(rest, num-1);
-      const attach = combinations.map((combination) => [v, ...combination]);
-      res.push(...attach);
-    })*/
     return res;
   }
