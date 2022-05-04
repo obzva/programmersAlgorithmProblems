735e4223612bc294e562498af2e9f484bbb63012
obzva <dngyng1000@gmail.com>
2022-05-04 11:25:48 +0900

    [ADD]

---
+++ difficulty 2/큰 수 만들기solution.js
@@ -0,0 +1,19 @@
+function solution(number, k) {
+  var answer = "";
+  let answerStack = [0];
+  let deleteCount = -1;
+  for (let i = 0; i < number.length; i++) {
+    while (deleteCount < k && number[i] > answerStack[answerStack.length - 1]) {
+      answerStack.pop();
+      deleteCount++;
+    }
+    if (answerStack.length < number.length - k) answerStack.push(number[i]);
+  }
+  answer = answerStack.join("");
+  return answer;
+}
+
+//testCase
+let number = "4177252841";
+let k = 4;
+console.log(solution(number, k));
