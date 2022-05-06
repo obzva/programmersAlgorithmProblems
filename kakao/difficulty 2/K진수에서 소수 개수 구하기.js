5458c8a4b63c505d4e92d4d59caf1283bf3bf82e
obzva <dngyng1000@gmail.com>
2022-05-06 19:32:32 +0900

    [ADD]

---
+++ kakao/difficulty 2/K진수에서 소수 개수 구하기.js
@@ -0,0 +1,24 @@
+const isPrime = (number) => {
+  if (number === 1) return false;
+  if (number === 2) return true;
+  const max = Math.sqrt(number);
+  for (let i = 2; i <= max; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+
+const solution = (n, k) => {
+  let res = 0;
+
+  const arr = n.toString(k).split(/0+/);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (isPrime(Number(arr[i]))) res++;
+  }
+  return res;
+};
+
+//testCase
+//const [n, k] = [110011, 10];
+//solution(n, k);
